563a3f072d34e62c98eeb22aa50647eb1581a976
pnck <hio131@gmail.com>
2017-09-15 11:05:54 +0800

    update CQSDK.py 调用SDK传入的参数转为str(gbk)

--- CQSDK.py
+++ CQSDK.py
@@ -1,89 +1,122 @@
-# -*- coding:gbk -*-
+# -*- coding:utf-8 -*-
 
 import _CQSDK
 
-EVENT_IGNORE        = 0     # ÊÂ¼ş_ºöÂÔ
-EVENT_BLOCK         = 1     # ÊÂ¼ş_À¹½Ø
+EVENT_IGNORE        = 0     # äº‹ä»¶_å¿½ç•¥
+EVENT_BLOCK         = 1     # äº‹ä»¶_æ‹¦æˆª
 
-REQUEST_ALLOW       = 1     # ÇëÇó_Í¨¹ı
-REQUEST_DENY        = 2     # ÇëÇó_¾Ü¾ø
+REQUEST_ALLOW       = 1     # è¯·æ±‚_é€šè¿‡
+REQUEST_DENY        = 2     # è¯·æ±‚_æ‹’ç»
 
-REQUEST_GROUPADD    = 1     # ÇëÇó_ÈºÌí¼Ó
-REQUEST_GROUPINVITE = 2     # ÇëÇó_ÈºÑûÇë
+REQUEST_GROUPADD    = 1     # è¯·æ±‚_ç¾¤æ·»åŠ 
+REQUEST_GROUPINVITE = 2     # è¯·æ±‚_ç¾¤é‚€è¯·
 
-CQLOG_DEBUG         = 0     # µ÷ÊÔ »ÒÉ«
-CQLOG_INFO          = 10    # ĞÅÏ¢ ºÚÉ«
-CQLOG_INFOSUCCESS   = 11    # ĞÅÏ¢(³É¹¦) ×ÏÉ«
-CQLOG_INFORECV      = 12    # ĞÅÏ¢(½ÓÊÕ) À¶É«
-CQLOG_INFOSEND      = 13    # ĞÅÏ¢(·¢ËÍ) ÂÌÉ«
-CQLOG_WARNING       = 20    # ¾¯¸æ ³ÈÉ«
-CQLOG_ERROR         = 30    # ´íÎó ºìÉ«
-CQLOG_FATAL         = 40    # ÖÂÃü´íÎó Éîºì
+CQLOG_DEBUG         = 0     # è°ƒè¯• ç°è‰²
+CQLOG_INFO          = 10    # ä¿¡æ¯ é»‘è‰²
+CQLOG_INFOSUCCESS   = 11    # ä¿¡æ¯(æˆåŠŸ) ç´«è‰²
+CQLOG_INFORECV      = 12    # ä¿¡æ¯(æ¥æ”¶) è“è‰²
+CQLOG_INFOSEND      = 13    # ä¿¡æ¯(å‘é€) ç»¿è‰²
+CQLOG_WARNING       = 20    # è­¦å‘Š æ©™è‰²
+CQLOG_ERROR         = 30    # é”™è¯¯ çº¢è‰²
+CQLOG_FATAL         = 40    # è‡´å‘½é”™è¯¯ æ·±çº¢
 
 
+def i_hate_unicode_with_windows(f):
+    def wrapper(*args):
+        newargs = []
+        for arg in args:
+            if type(arg) is type(u'unicode'):
+                arg = arg.encode('gbk')
+            newargs.append(arg)    
+        return f(*newargs)
+    return wrapper
+            
+
+@i_hate_unicode_with_windows
 def MessageBox(msg, title):
     return _CQSDK.MessageBox(msg, title)
 
+@i_hate_unicode_with_windows
 def GetAppID():
     return _CQSDK.GetAppID()
 
+@i_hate_unicode_with_windows
 def SendPrivateMsg(QQID, msg):
     return _CQSDK.SendPrivateMsg(QQID, msg)
 
+@i_hate_unicode_with_windows
 def SendGroupMsg(groupid, msg):
     return _CQSDK.SendGroupMsg(groupid, msg)
 
+@i_hate_unicode_with_windows
 def SendDiscussMsg(discussid, msg):
     return _CQSDK.SendDiscussMsg(discussid, msg)
 
+@i_hate_unicode_with_windows
 def SendLike(QQID):
     return _CQSDK.SendLike(QQID)
 
+@i_hate_unicode_with_windows
 def SetGroupKick(groupid, QQID, rejectaddrequest):
     return _CQSDK.SetGroupKick(groupid, QQID, rejectaddrequest)
 
+@i_hate_unicode_with_windows
 def SetGroupBan(groupid, QQID, duration):
     return _CQSDK.SetGroupBan(groupid, QQID, duration)
 
+@i_hate_unicode_with_windows
 def SetGroupAdmin(groupid, QQID, setadmin):
     return _CQSDK.SetGroupAdmin(groupid, QQID, setadmin)
 
+@i_hate_unicode_with_windows
 def SetGroupWholeBan(groupid, enableban):
     return _CQSDK.SetGroupWholeBan(groupid, enableban)
 
+@i_hate_unicode_with_windows
 def SetGroupAnonymousBan(groupid, anomymous, duration):
     return _CQSDK.SetGroupAnonymousBan(groupid, anomymous, duration)
 
+@i_hate_unicode_with_windows
 def SetGroupAnonymous(groupid, enableanomymous):
     return _CQSDK.SetGroupAnonymous(groupid, enableanomymous)
 
+@i_hate_unicode_with_windows
 def SetGroupCard(groupid, QQID, newcard):
     return _CQSDK.SetGroupCard(groupid, QQID, newcard)
 
+@i_hate_unicode_with_windows
 def SetGroupLeave(groupid, isdismiss):
     return _CQSDK.SetGroupLeave(groupid, isdismiss)
 
+@i_hate_unicode_with_windows
 def SetGroupSpecialTitle(groupid, QQID, newspecialtitle, duration):
     return _CQSDK.SetGroupSpecialTitle(groupid, QQID, newspecialtitle, duration)
 
+@i_hate_unicode_with_windows
 def SetDiscussLeave(discussid):
     return _CQSDK.SetDiscussLeave(discussid)
 
+@i_hate_unicode_with_windows
 def SetFriendAddRequest(responseflag, responseoperation, remark):
     return _CQSDK.SetFriendAddRequest(responseflag, responseoperation, remark)
 
+@i_hate_unicode_with_windows
 def SetGroupAddRequestV2(responseflag, requesttype, responseoperation, reason):
     return _CQSDK.SetGroupAddRequestV2(responseflag, requesttype, responseoperation, reason)
 
+@i_hate_unicode_with_windows
 def GetGroupMemberInfoV2(groupid, QQID, nocache = False):
     return _CQSDK.GetGroupMemberInfoV2(groupid, QQID, nocache)
 
+@i_hate_unicode_with_windows
 def GetGroupMemberList(groupid):
     return _CQSDK.GetGroupMemberList(groupid)
     
+@i_hate_unicode_with_windows
 def GetStrangerInfo(QQID, nocache = False):
     return _CQSDK.GetStrangerInfo(QQID, nocache)
 
+@i_hate_unicode_with_windows
 def AddLog(priority, category, content):
     return _CQSDK.AddLog(priority, category, content)
 
